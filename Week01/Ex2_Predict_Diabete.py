24830a4a8f38853480c7b7648280d19241be1288
Anh-Huy Dinh <anhhuy.anthoi@gmail.com>
2020-10-15 23:21:41 +0700

    sua bai 2, 3

--- Week01/Ex2_Predict_Diabete.py
+++ Week01/Ex2_Predict_Diabete.py
@@ -48,9 +48,9 @@ scores = cross_val_score(clf, X, y, cv=5)
 print("scores = {} \n final score = {} \n".format(scores, scores.mean()))
 
 ## 5. Building Random Forest Model ##
-# Create Decision Tree classifier object
+# Create Random Forest object
 rdf = RandomForestClassifier()
-# Train Decision Tree Classifier
+# Train Random Forest Classifier
 rdf = rdf.fit(X_train, y_train)
 # Predict the response for test dataset
 y_pred = rdf.predict(X_test)
@@ -60,19 +60,55 @@ print("Accuracy of random forest: ", metrics.accuracy_score(y_test, y_pred))
 scores = cross_val_score(rdf, X, y, cv=5)
 print("scores = {} \n final score = {} \n".format(scores, scores.mean()))
 
-
 ## 6. Building Logistic Regression Model ##
-# Create Decision Tree classifier object
+# Create Logistic Regression object
 lr = LogisticRegression(max_iter=2000)
-# Train Decision Tree Classifier
+# Train Logistic Regression
 lr = lr.fit(X_train, y_train)
 # Predict the response for test dataset
 y_pred = lr.predict(X_test)
-
-## Evaluating Logistic Regression Model ##
 # Model Accuracy, how often is the classifier corret?
 print("Accuracy of logistic regression: ", metrics.accuracy_score(y_test, y_pred))
 # Evaluate a score by cross validation
 scores = cross_val_score(lr, X, y, cv=5)
 print("scores = {} \n final score = {} \n".format(scores, scores.mean()))
 
+## 7. Building Ada Boost Classifier Model ##
+# Create Ada Boost Classifier object
+abc = AdaBoostClassifier()
+# Train Ada Boost Classifier Classifier
+abc = abc.fit(X_train, y_train)
+# Predict the response for test dataset
+y_pred = abc.predict(X_test)
+# Model Accuracy, how often is the classifier corret?
+print("Accuracy of ada boost classifier: ", metrics.accuracy_score(y_test, y_pred))
+# Evaluate a score by cross validation
+scores = cross_val_score(abc, X, y, cv=5)
+print("scores = {} \n final score = {} \n".format(scores, scores.mean()))
+
+## 7. Building SVC Model ##
+# Create SVC object
+svc = SVC()
+# Train SVC
+svc = svc.fit(X_train, y_train)
+# Predict the response for test dataset
+y_pred = svc.predict(X_test)
+# Model Accuracy, how often is the classifier corret?
+print("Accuracy of SVC: ", metrics.accuracy_score(y_test, y_pred))
+# Evaluate a score by cross validation
+scores = cross_val_score(svc, X, y, cv=5)
+print("scores = {} \n final score = {} \n".format(scores, scores.mean()))
+
+MLPClassifier
+## 8. Building MLP classifier Model ##
+# Create MLP object
+mlp = MLPClassifier()
+# Train MLP
+mlp = mlp.fit(X_train, y_train)
+# Predict the response for test dataset
+y_pred = mlp.predict(X_test)
+# Model Accuracy, how often is the classifier corret?
+print("Accuracy of MLP classifier: ", metrics.accuracy_score(y_test, y_pred))
+# Evaluate a score by cross validation
+scores = cross_val_score(svc, X, y, cv=5)
+print("scores = {} \n final score = {} \n".format(scores, scores.mean()))
