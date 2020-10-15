c61170b5a9f3780fe9100c2da44a0a7ba7bbc00e
Anh-Huy Dinh <anhhuy.anthoi@gmail.com>
2020-10-15 14:02:26 +0700

    sua ex2

--- Week01/Ex2_Predict_Diabete.py
+++ Week01/Ex2_Predict_Diabete.py
@@ -11,7 +11,7 @@ from sklearn import metrics
 import eli5
 from eli5.sklearn import PermutationImportance
 
-from sklearn.externals.six import StringIO
+#from sklearn.externals.six import StringIO
 from IPython.display import Image
 import pydotplus
 
@@ -50,3 +50,8 @@ eli5.show_weights(perm, feature_names = X_test.columns.tolist())
 print("Accuracy: ", metrics.accuracy_score(y_test, y_pred))
 
 ## 6. Visualizing Decision Trees ##
+# dot_data = StringIO()
+# export_graphviz(clf, out_file=dot_data, filled=True, rounded=True, special_characters=True, feature_names=feature_cols, class_names=['0', '1'])
+# graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+# graph.write_png('diabetes.png')
+# Image(graph.create_png())
